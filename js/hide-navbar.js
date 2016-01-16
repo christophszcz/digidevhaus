0aa07b79f24f8459f1bd052b6a490260eda0977d
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2016-01-15 22:33:11 -0500

    Added ability to hide the navbar when scrolling down

---
+++ js/hide-navbar.js
@@ -0,0 +1,13 @@
+(function() {
+  var documentElem = $(document),
+  nav = $('nav'),
+  lastScrollTop = 0;
+
+  documentElem.on('scroll', function() {
+    var currentScrollTop = $(this).scrollTop();
+    if ( currentScrollTop > lastScrollTop ) nav.addClass('hidden');
+    else nav.removeClass('hidden');
+    lastScrollTop = currentScrollTop;
+  });
+
+})();
