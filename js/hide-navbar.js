cb7e055ddb05f7d6374922c0651a4129b1130902
christophszcz <christoph.szczechowicz@gmail.com>
2016-12-17 23:59:09 -0500

    Added space under the navbar before section title.

--- js/hide-navbar.js
+++ js/hide-navbar.js
@@ -1,14 +1,14 @@
-(function() {
-  var documentElem = $(document),
-  nav = $('nav'),
-  lastScrollTop = 0;
+// (function() {
+//   var documentElem = $(document),
+//   nav = $('nav'),
+//   lastScrollTop = 0;
 
-  documentElem.on('scroll', function() {
-    var currentScrollTop = $(this).scrollTop();
-    if ( currentScrollTop > lastScrollTop ) nav.addClass('hidden');
-    else nav.removeClass('hidden');
-    lastScrollTop = currentScrollTop;
-    $('.navbar').css('background-color', '#000');
-  });
+//   documentElem.on('scroll', function() {
+//     var currentScrollTop = $(this).scrollTop();
+//     if ( currentScrollTop > lastScrollTop ) nav.addClass('hidden');
+//     else nav.removeClass('hidden');
+//     lastScrollTop = currentScrollTop;
+//     $('.navbar').css('background-color', '#000');
+//   });
 
-})();
+// })();
