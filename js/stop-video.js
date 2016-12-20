40fb6a95646c2c358257599447d884ef7f7a2cc3
christophszcz <christoph.szczechowicz@gmail.com>
2016-12-19 23:59:22 -0500

    The pause happens lower.

--- js/stop-video.js
+++ js/stop-video.js
@@ -1,12 +1,13 @@
 $(window).scroll(function() {
 	var aboutScroll = $('#about-scroll').offset().top,
+    serviceScroll = $('#services-scroll').offset().top,
 	 	margin = $('#about-scroll').outerHeight(),
     windowHeight = $(window).height(),
     windowScroll = $(this).scrollTop();
   if (windowScroll < (aboutScroll + margin - windowHeight)){ 
     $("video")[0].play();
   }
-  if (windowScroll > (aboutScroll + margin - windowHeight)){
+  if (windowScroll > (serviceScroll + margin - windowHeight)){
   	$("video")[0].pause();
   }
 });
